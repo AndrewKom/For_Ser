cb393a340844d0df112f47b37cd17721b79b6e7d
AndrewKom <sinhron2020@yandex.ru>
2024-11-19 19:51:12 +0300

    del bad result

--- Semester 4/lab2/lab2_p.py
+++ Semester 4/lab2/lab2_p.py
@@ -11,6 +11,7 @@ from keras import initializers
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
+import math as m
 
 
 
@@ -65,6 +66,14 @@ true_df = pd.DataFrame(Y_test, columns=['psi1_true', 'u1_true', 'u2_true'])
 result_df = pd.concat([input_df, true_df, predictions_df], axis=1)
 
 
+maxtime = 0.15
+result_df = result_df.drop( result_df[(((result_df['u1_true']) - (result_df['u1_pred'])) >= maxtime) |
+                                      (((result_df['u2_true']) - (result_df['u2_pred'])) >= maxtime) |
+                                      (((result_df['u1_pred']) - (result_df['u1_true'])) >= maxtime) |
+                                      (((result_df['u2_pred']) - (result_df['u2_true'])) >= maxtime)].index)
+
+
+
 print("\nPredictions:")
 print(result_df)
 
@@ -73,10 +82,11 @@ print(result_df)
 #Отрисовка графики u1
 fig = plt.figure()
 ax = plt.axes(projection='3d')
+ax.set_zlim(0, 1)
 
 ax.scatter(result_df["q1"], result_df["q2"], result_df["u1_pred"], c="red")
 ax.scatter(result_df["q1"], result_df["q2"], result_df["u1_true"], c="blue")
-# syntax for plotting
+
 ax.set_title('U1')
 plt.xlabel("q1")
 plt.ylabel("q2")
@@ -86,10 +96,11 @@ plt.show()
 #Отрисовка графики u1
 fig = plt.figure()
 ax = plt.axes(projection='3d')
+ax.set_zlim(0, 1)
 
 ax.scatter(result_df["q1"], result_df["q2"], result_df["u2_pred"], c="red")
 ax.scatter(result_df["q1"], result_df["q2"], result_df["u2_true"], c="blue")
-# syntax for plotting
+
 ax.set_title('U2')
 plt.xlabel("q1")
 plt.ylabel("q2")
